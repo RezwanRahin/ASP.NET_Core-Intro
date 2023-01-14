5987676da56e6a3716ccea36d0cc4649065ee104
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-14 13:49:10 +0600

    added dummy 'Tutorial' instances and pass them as 'ViewData' in 'Tutorial' controller

--- Asp.netCoreMVCIntro/Controllers/TutorialController.cs
+++ Asp.netCoreMVCIntro/Controllers/TutorialController.cs
@@ -1,3 +1,4 @@
+using Asp.netCoreMVCIntro.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.netCoreMVCIntro.Controllers;
@@ -7,6 +8,13 @@ public class TutorialController : Controller
     // GET
     public IActionResult Index()
     {
+        var tutorials = new List<Tutorial>
+        {
+            new Tutorial { Id = 1, Name = "C#", Description = "C# tutorial" },
+            new Tutorial { Id = 2, Name = "Asp.net core", Description = "Asp.net core tutorial" }
+        };
+
+        ViewData["tutorials"] = tutorials;
         return View();
     }
 }
