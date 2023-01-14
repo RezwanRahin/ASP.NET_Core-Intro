54b34accf69dfe8f501e46c300f1e982413a637f
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-14 13:21:32 +0600

    added 3 properties to the 'Tutorial' model

--- Asp.netCoreMVCIntro/Models/Tutorial.cs
+++ Asp.netCoreMVCIntro/Models/Tutorial.cs
@@ -2,5 +2,7 @@ namespace Asp.netCoreMVCIntro.Models;
 
 public class Tutorial
 {
-    
+    public int Id { get; set; }   
+    public string Name { get; set; }
+    public string Description { get; set; }
 }
