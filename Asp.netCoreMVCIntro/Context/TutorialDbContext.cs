2cf545fcba879bcb953ec9277eb82aa81bd3383b
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-15 18:19:47 +0600

    passed the necessary configration information for DbContext in 'TutorialDbContext' class

--- Asp.netCoreMVCIntro/Context/TutorialDbContext.cs
+++ Asp.netCoreMVCIntro/Context/TutorialDbContext.cs
@@ -5,5 +5,9 @@ namespace Asp.netCoreMVCIntro.Context;
 
 public class TutorialDbContext : DbContext
 {
+    public TutorialDbContext(DbContextOptions<TutorialDbContext> options) : base(options)
+    { 
+    }
+    
     public DbSet<Tutorial> Tutorials { get; set; }
 }
