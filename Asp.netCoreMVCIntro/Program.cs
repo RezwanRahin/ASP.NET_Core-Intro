e299e55595d77f4a88b2eb3f0db14fe4344abeb4
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-15 16:14:37 +0600

    added a service to the dependency injection container in Program file of the project

--- Asp.netCoreMVCIntro/Program.cs
+++ Asp.netCoreMVCIntro/Program.cs
@@ -1,7 +1,10 @@
+using Asp.netCoreMVCIntro.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<ITutorialRepository, TutorialRepository>();
 
 var app = builder.Build();
 
