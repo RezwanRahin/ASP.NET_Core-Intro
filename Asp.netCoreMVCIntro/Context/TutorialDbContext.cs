b085c76316d2f3ba549e5bb782ff7889048f030a
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-15 18:13:43 +0600

    added a property of type 'DbSet<Tutorial>' in TutorialDbContext

--- Asp.netCoreMVCIntro/Context/TutorialDbContext.cs
+++ Asp.netCoreMVCIntro/Context/TutorialDbContext.cs
@@ -1,6 +1,9 @@
+using Asp.netCoreMVCIntro.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace Asp.netCoreMVCIntro.Context;
 
-public class TutorialDbContext
+public class TutorialDbContext : DbContext
 {
-    
+    public DbSet<Tutorial> Tutorials { get; set; }
 }
