43c93a05c5e91195e682869dcc75683a35dec75e
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-15 16:00:28 +0600

    added mock data and a property, also implemented methods in 'TutorialRepository' class implementing 'ITutorialRepository' interface

--- Asp.netCoreMVCIntro/Repository/TutorialRepository.cs
+++ Asp.netCoreMVCIntro/Repository/TutorialRepository.cs
@@ -1,6 +1,43 @@
+using Asp.netCoreMVCIntro.Models;
+
 namespace Asp.netCoreMVCIntro.Repository;
 
-public class TutorialRepository
+public class TutorialRepository : ITutorialRepository
 {
-    
+    private List<Tutorial> _tutorials;
+
+    public TutorialRepository()
+    {
+        // Temporaryily we are going to create mock data
+        _tutorials = new List<Tutorial>()
+        {
+            new Tutorial { Id = 1, Name = "C#", Description = "C# tutorial" },
+            new Tutorial { Id = 2, Name = "Asp.net core", Description = "Asp.net core tutorial" }
+        };
+    }
+
+    public void Add(Tutorial tutorial)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Tutorial Update(Tutorial tutorial)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Tutorial GetTutorial(int Id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Tutorial> GetAllTutorial()
+    {
+        return _tutorials;
+    }
 }
