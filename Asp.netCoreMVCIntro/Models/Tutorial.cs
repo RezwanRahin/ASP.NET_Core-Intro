a1d48588efb458c364ba234119060db389187311
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-18 18:14:49 +0600

    changed the display name of the attributes in 'Tutorial' model class

--- Asp.netCoreMVCIntro/Models/Tutorial.cs
+++ Asp.netCoreMVCIntro/Models/Tutorial.cs
@@ -7,9 +7,11 @@ public class Tutorial
     public int Id { get; set; }
 
     [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z_]*$", ErrorMessage = "Please enter text")]
-    [Required]   
-    public string Name { get; set; }
-    
     [Required]
+    [Display(Name = "Tutorial Name")]   
+    public string Name { get; set; }
+
+    [Required]
+    [Display(Name = "Tutorial Description")]
     public string Description { get; set; }
 }
