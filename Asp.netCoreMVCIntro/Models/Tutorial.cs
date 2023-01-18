e6b4e9471ffceadc2d9fbb1a8d084f5fdfcf40e6
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-01-18 16:44:54 +0600

    added validation constraints to the properties of 'Tutorial' model class

--- Asp.netCoreMVCIntro/Models/Tutorial.cs
+++ Asp.netCoreMVCIntro/Models/Tutorial.cs
@@ -1,8 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Asp.netCoreMVCIntro.Models;
 
 public class Tutorial
 {
-    public int Id { get; set; }   
+    public int Id { get; set; }
+
+    [RegularExpression(@"^[a-zA-Z]+[ a-zA-Z_]*$", ErrorMessage = "Please enter text")]
+    [Required]   
     public string Name { get; set; }
+    
+    [Required]
     public string Description { get; set; }
 }
