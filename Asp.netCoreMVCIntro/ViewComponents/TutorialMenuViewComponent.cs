ca3ed6f23598baf0900cf610e101e476fd396369
Rezwan Rahin <rezwanshahriorrahin@gmail.com>
2023-03-25 15:28:05 +0600

    injected 'ITutorialRepository' via constructor injection, thus adding a private property, in TutorialMenuViewComponent class

--- Asp.netCoreMVCIntro/ViewComponents/TutorialMenuViewComponent.cs
+++ Asp.netCoreMVCIntro/ViewComponents/TutorialMenuViewComponent.cs
@@ -1,8 +1,14 @@
+using Asp.netCoreMVCIntro.Repository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.netCoreMVCIntro.ViewComponents;
 
 public class TutorialMenuViewComponent : ViewComponent
 {
-    
+    private readonly ITutorialRepository _tutorialRepository;
+
+    public TutorialMenuViewComponent(ITutorialRepository tutorialRepository)
+    {
+        _tutorialRepository = tutorialRepository;
+    }   
 }
