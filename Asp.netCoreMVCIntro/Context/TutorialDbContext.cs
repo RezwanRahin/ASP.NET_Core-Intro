78ab24f06c89d6410efe6cd535d8fa322cd7bfea
rezwan <rezwanshahriorrahin@gmail.com>
2023-05-09 13:37:09 +0600

    added a property of type 'DbSet<Article>' in 'TutorialDbContext' class

--- Asp.netCoreMVCIntro/Context/TutorialDbContext.cs
+++ Asp.netCoreMVCIntro/Context/TutorialDbContext.cs
@@ -8,6 +8,7 @@ public class TutorialDbContext : DbContext
     public TutorialDbContext(DbContextOptions<TutorialDbContext> options) : base(options)
     { 
     }
-    
+
     public DbSet<Tutorial> Tutorials { get; set; }
+    public DbSet<Article> Articles { get; set; }
 }
