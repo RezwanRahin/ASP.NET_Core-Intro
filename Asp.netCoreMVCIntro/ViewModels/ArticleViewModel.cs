a6dc2bdf09ba7c8fd41897be065f493afd512313
rezwan <rezwanshahriorrahin@gmail.com>
2023-07-24 12:10:12 +0600

    created a new ViewModel named 'ArticleViewModel'

---
+++ Asp.netCoreMVCIntro/ViewModels/ArticleViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Asp.netCoreMVCIntro.Models.ViewModels;
+
+public class ArticleViewModel
+{
+    public List<Tutorial> tutorials { get; set; }
+    
+    public ArticleViewModel()
+    {
+        tutorials = new List<Tutorial>();
+    }
+
+    public int Id { get; set; }
+
+    [Required(ErrorMessage = "Please enter article title")]
+    [Display(Name = "Please enter article title")]
+    public string ArticleTitle { get; set; }
+
+    [Required(ErrorMessage = "Please enter article content")]
+    [Display(Name = "Write article")]
+    public string ArticleContent { get; set; }
+
+    public int TutorialId { get; set; }
+}
